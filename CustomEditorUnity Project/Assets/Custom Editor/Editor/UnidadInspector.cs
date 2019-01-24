6e4f4b3dab763209d065a7eb2b0ef3332a196fd0
Moon Antonio <pinchodev91@gmail.com>
2019-01-24 16:29:48 +0100

    Inspector

---
+++ CustomEditorUnity Project/Assets/Custom Editor/Editor/UnidadInspector.cs
@@ -0,0 +1,53 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// UnidadInspector.cs (24/01/2019)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Inspector de la clase Unidad.								\\
+// Fecha Mod:		24/01/2019													\\
+// Ultima Mod:		Version Inicial.											\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEditor;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Inspector de la clase <see cref="Unidad"/>.</para>
+	/// </summary>
+	[HelpURL("https://moonantonio.github.io/"), AddComponentMenu("MoonAntonio/UnidadInspector"), CustomEditor(typeof(Unidad))]
+	public class UnidadInspector : Editor 
+	{
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+
+			EditorGUILayout.LabelField("Datos Unidad");
+			SerializedProperty dataProperty = serializedObject.FindProperty("data");
+			SerializedProperty vidaProperty = dataProperty.FindPropertyRelative("vida");
+			SerializedProperty manaProperty = dataProperty.FindPropertyRelative("mana");
+			SerializedProperty isVivoProperty = dataProperty.FindPropertyRelative("isVivo");
+
+			EditorGUILayout.BeginHorizontal("box");
+			vidaProperty.floatValue = EditorGUILayout.FloatField("HP", vidaProperty.floatValue);
+			manaProperty.floatValue = EditorGUILayout.FloatField("MP" ,manaProperty.floatValue);
+			EditorGUILayout.EndHorizontal();
+
+			EditorGUILayout.BeginHorizontal("box");
+			isVivoProperty.boolValue = EditorGUILayout.Toggle("Alive", isVivoProperty.boolValue);
+			EditorGUILayout.EndHorizontal();
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		#region Dev
+		[ContextMenu("Dev")]
+	    protected void DevScript()
+	    {
+	        Debug.Log("<color=green>UnidadInspector :: DevScript()</color> >> Inicializado.");
+	    }
+	    #endregion
+	}
+}
---
+++ CustomEditorUnity Project/Assets/Custom Editor/Scripts/Unidad.cs
@@ -0,0 +1,44 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Unidad.cs (24/01/2019)														\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Clase que contiene la informacion de una unidad.			\\
+// Fecha Mod:		24/01/2019													\\
+// Ultima Mod:		Version Inicial.											\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Clase que contiene la informacion de una unidad.</para>
+	/// </summary>
+	[HelpURL("https://moonantonio.github.io/"), AddComponentMenu("MoonAntonio/Unidad"), System.Serializable]
+	public class Unidad : MonoBehaviour 
+	{
+		#region Variables Publicas
+		public Data data;
+		#endregion
+
+		#region Dev
+		[ContextMenu("Dev")]
+	    protected void DevScript()
+	    {
+	        Debug.Log("<color=green>Unidad :: DevScript()</color> >> Inicializado.");
+	    }
+	    #endregion
+	}
+
+	[System.Serializable]
+	public class Data
+	{
+		#region Variables Publicas
+		public float vida = 0.0f;
+		public float mana = 0.0f;
+		public bool isVivo = false;
+		#endregion
+	}
+}
